4d742291057ce7d4fd6a712306d0670b7df79fc0
MuhammedAnaz <muhammedanas8587@gmail.com>
2023-06-10 20:13:08 +0530

    second commit

--- app1/urls.py
+++ app1/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('portfoliodetails', views.portfoliodetails),
     path('about', views.about),
-    path('logn',views.login),
+    path('logn',views.userlogin),
     path('services',views.services),
     path('portfolio',views.portfolio),
     path('team',views.team),
@@ -14,14 +14,30 @@ urlpatterns = [
     path('register',views.register),
     path('log',views.log),
     path('events',views.events),
+    path('userlogout',views.userlogout),
     path('eventsform',views.eventsform),
     path('events_data_store',views.events_data_store),
     path('bookingdetails',views.bookingdetails),
     path('contact',views.contact),
     path('adminlogin',views.adminlogin),
-    path('adminlogin_view',views.adminlogin_view)
+    path('adminlogin_view',views.adminlogin_view),
+    path('admindashboard',views.admindashboard),
+    path('delete_booking/<int:id>/', views.delete_booking, name='delete_booking'),
+    path('deletebookingpage',views.deletebookingpage),
+    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('admin_logout',views.admin_logout),
+    path('contactdetails',views.contactdetails),
+    path('edit_contact/<int:contact_id>/', views.edit_contact, name='edit_contact'),
+    path('delete_contact/<int:id>/', views.delete_contact, name='delete_contact'),
+    path('Registrationdetails',views.Registrationdetails),
+    path('edit_registration/<int:reg_id>/', views.edit_registration, name='edit_registration'),
+    path('delete_registration/<int:id>/',views.delete_registration, name='delete_registration')
+
+
 
 ]
+
+
 
     
  
--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from app1.models import Registration, Booking,Contact
 from django.http import HttpResponse
 from django.contrib.sessions.models import Session
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 
 
 
@@ -32,7 +32,7 @@ def team(request):
 def contactpage(request):
     return render(request, 'contact.html')  
 
-def login(request):
+def userlogin(request):
     return render(request,'login.html')
 
 def registration(request):
@@ -62,6 +62,10 @@ def log(request):
             messages.error(request, error_message)
     # If the code reaches this point, either it's a GET request or the login failed
     return render(request, 'login.html')
+
+def userlogout(request):
+    Session.objects.all().delete()
+    return render(request,'login.html')
 
 def events(request):
     return render(request, 'events.html')
@@ -100,46 +104,124 @@ def contact(request):
 def adminlogin(request):
     return render(request,'adminlogin.html')    
 
-# def adminlogin_view(request):
-#     # Check if it's a POST request
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-
-#         # Authenticate the user
-#         user = authenticate(request, username=username, password=password)
-
-#         # Check if authentication was successful
-#         if user is not None:
-#             # Login the user
-#             login(request, user)
-#             return HttpResponse('Login Successful')
-#         else:
-#             return HttpResponse('Invalid Login Credentials')
-#     else:
-#         # Render the login form
-#         return render(request, 'login.html')   
 
 
 def adminlogin_view(request):
     if request.method == 'POST':
+        # Retrieve username and password from the submitted form
         username = request.POST.get('username')
         password = request.POST.get('password')
+    
+        # Authenticate the user with the provided credentials
         user = authenticate(request, username=username, password=password)
-        if user is not None and user.is_superuser:
+        
+        if user is not None:
+            # If authentication is successful, log in the user
             login(request, user)
-            return redirect('index.html')  # Redirect to the admin dashboard page
+            return redirect('/admindashboard')  # Redirect to the admin dashboard page
         else:
+            # If authentication fails, set an error message
             error_message = 'Invalid credentials'
     else:
+        # If it's not a POST request, initialize error_message as None
         error_message = None
 
+    # Render the adminlogin.html template, passing the error_message to display in the template
     return render(request, 'adminlogin.html', {'error_message': error_message})
 
+def admindashboard(request):
+    return render(request,'admindashboard.html')
+def deletebookingpage(request):
+    return render(request,'delete_booking.html')
 
+def delete_booking(request, id):
+    # Retrieve the booking object based on the provided id
+    booking = Booking.objects.get(pk=id)
+
+    if request.method == 'POST':
+        # If it's a POST request, delete the booking object from the database
+        booking.delete()
+        return redirect('/bookingdetails')  # Redirect to the booking details page
+
+    # If it's not a POST request, render the delete_booking.html template
+    # Pass the booking object to the template to display the details
+    return render(request, 'delete_booking.html', {'booking': booking})
+
+
+def edit_booking(request, booking_id):
+    # Retrieve the booking object with the specified ID or return a 404 error
+    booking = get_object_or_404(Booking, id=booking_id)
     
-        
+    if request.method == 'POST':
+        # Update the booking object with the new values from the form
+        booking.name = request.POST.get('name')
+        booking.phone = request.POST.get('phone')
+        booking.email = request.POST.get('email')
+        booking.event = request.POST.get('event')
+        booking.number_of_persons = request.POST.get('number_of_persons')
+        booking.price = request.POST.get('price')
+        booking.totalprice = request.POST.get('totalprice')
+        booking.save()  # Save the updated booking object to the database
+        # Redirect to the booking details page for the updated booking
+        return redirect('/bookingdetails', booking_id=booking.id)
+    
+    # Render the edit booking form template with the booking object
+    return render(request, 'edit_booking.html', {'booking': booking})
 
+def admin_logout(request):
+    logout(request)
+    return redirect('/adminlogin')  # Replace 'admin_login' with the appropriate URL name for your admin login page    
+def contactdetails(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contactdetails.html', {'contacts':contacts })
 
+def edit_contact(request, contact_id):
+    # Retrieve the contact object with the specified ID or return a 404 error
+    contact = get_object_or_404(Contact, id=contact_id)
 
+    if request.method == 'POST':
+        # Update the contact object with the new values from the form
+        contact.name = request.POST.get('name')
+        contact.email = request.POST.get('email')
+        contact.subject = request.POST.get('subject')
+        contact.message = request.POST.get('message')
+        contact.save()  # Save the updated contact object to the database
+        # Redirect to the contact details page for the updated contact
+        return redirect('/contactdetails', contact_id=contact.id)
 
+    # Render the edit contact form template with the contact object
+    return render(request, 'editcontact.html', {'contact': contact})
+def delete_contact(request, id):
+    # Retrieve the booking object based on the provided id
+    contact = Contact.objects.get(pk=id)
+    if request.method == 'POST':
+        # If it's a POST request, delete the booking object from the database
+        contact.delete()
+        return redirect('/contactdetails')  # Redirect to the booking details page
+
+    # If it's not a POST request, render the delete_booking.html template
+    # Pass the booking object to the template to display the details
+    return render(request, 'deletecontact.html', {'contact': contact})
+def Registrationdetails(request):
+    reg = Registration.objects.all()
+    return render(request, 'registrationdetails.html', {'reg':reg })
+
+def edit_registration(request, reg_id):
+    reg = get_object_or_404(Registration, id=reg_id)
+    if request.method == 'POST':
+        reg.Name = request.POST.get('name')
+        reg.Age = request.POST.get('age')
+        reg.Email = request.POST.get('email')
+        reg.Phone_number = request.POST.get('phone')
+        reg.Password = request.POST.get('password')
+        reg.save()  
+        return redirect('/Registrationdetails', reg_id=reg.id)
+    return render(request, 'editregistration.html', {'reg': reg})
+
+def delete_registration(request, id):
+    # Retrieve the booking object based on the provided id
+    reg = Registration.objects.get(pk=id)
+    if request.method == 'POST':
+        reg.delete()
+        return redirect('/Registrationdetails') 
+    return render(request, 'deleteregistration.html', {'reg': reg})         
